370545afb330e6c04ade218155cc1d12fcf9ea65
CollinBarlow <collinbarlow@gmail.com>
2023-03-24 16:18:03 -0700

    updating for end of day

--- express-Petvana/routes/index.js
+++ express-Petvana/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('homePage', { title: 'Express' });
+  res.render('homePage', { title: 'Petvana' });
 });
 
 module.exports = router;
