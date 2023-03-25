eaa44453503faac9ab032ebd29a0e97d024cc88f
CollinBarlow <collinbarlow@gmail.com>
2023-03-25 09:59:52 -0700

    adding files for controllers, routes, and model

---
+++ express-Petvana/models/health.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+
+module.exports = mongoose.model('Health', healthSchema);
