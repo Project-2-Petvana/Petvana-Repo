916b9e676a5e1e378cea7733a5b27186260aa6c6
CollinBarlow <collinbarlow@gmail.com>
2023-03-27 15:38:22 -0700

    including OAUTH

--- express-Petvana/server.js
+++ express-Petvana/server.js
@@ -4,12 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var passport = require('passport');
+var session = require('express-session');
+var passport = require('passport');
 // Add the line below 
-// var methodOverride = require('method-override');
+var methodOverride = require('method-override');
 
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
@@ -25,9 +27,23 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
