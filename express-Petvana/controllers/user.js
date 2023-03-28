d76d57fbeccf199f9626a84a85cdab3854727cad
Sally-Kam <skam2000@gmail.com>
2023-03-28 13:25:51 -0400

    updated createPet function and index function and user.ejs

--- express-Petvana/controllers/user.js
+++ express-Petvana/controllers/user.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const Pet = require('../models/pet');
 // const pet = require('../models/pet');
-const pet = require('../models/pet');
+// const pet = require('../models/pet');
 
 
 module.exports = {
@@ -14,7 +14,7 @@ module.exports = {
 // This will retrieve user information and show a list of pets belonging to the user, rendering the user.ejs
 async function index(req, res) {
 try {
-const user = await User.find({});
+const user = await User.findById(req.user.id).populate('pet');
 // const pet = await Pet.find({user: pet._id});
     res.render('user/user', {title: 'User Profile', user});
 } catch(err) {
@@ -32,30 +32,37 @@ async function newPet(req, res) {
 
 //This will submit the pet's information via a form on the addPets/ejs view, redirecting to profile.ejs
 async function createPet(req, res) {
-try {
-    const user = await User.findById(req.params.id);
-    // console.log(req.params.id, "id")
-    // console.log(req.body.user, "user")
-    // console.log(req.body)
-    // console.log(user)
-    // req.body.user = user._id;
-    // console.log(req.body.user, "user test")
-    // req.body.name = req.user.name;
-    // console.log(req.body.name, "userName")
-    // req.body.avatar = req.user.avatar;
-    // console.log(req.body.avatar, "avatar")
-    // console.log(req.body)
-
-    user.pet.push(req.body);
-    console.log(user.pet, "pet")
-    console.log(req.body, "body")
-    await user.save();
-    // await Pet.create(req.body);
-    res.redirect('/user');
-} catch(err) {
-    res.sendStatus(500)
+    try {
+      const user = await User.findById(req.user._id); // Get the currently authenticated user
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      const { name, species, age, sex } = req.body;
+      if (!name || !species || !age || !sex) {
+        return res.status(400).json({ error: 'Missing required fields' });
+      }
+  
+      const pet = new Pet({
+        name,
+        species,
+        age,
+        sex,
+        user: user._id
+      });
+  
+      await pet.save();
+      user.pet.push(pet._id);
+      await user.save();
+      console.log(pet)
+      console.log(user)
+  
+      res.redirect('/user');
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
 }
-};
 
 // This will delete the pet and all their health data from the database, redirecting to user.ejs
 async function deletePet(req, res) {
@@ -70,4 +77,4 @@ async function deletePet(req, res) {
     } catch(err) {
         return next(err);
     }
-};
+}
