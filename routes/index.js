fd6d27be59aaa5eb26f77b6b1b20572099562b61
Seraphiel97 <seth.graves@outlook.com>
2023-03-30 17:51:58 -0400

    complete comments on server

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('homePage', { title: 'Petvana' });
@@ -18,8 +17,6 @@ router.get('/auth/google', passport.authenticate(
   {
     // Requesting the user's profile and email
     scope: ['profile', 'email'],
-    // Optionally force pick account every time
-    // prompt: "select_account"
   }
 ));
 
@@ -29,7 +26,6 @@ router.get('/oauth2callback', passport.authenticate(
   'google',
   {
     successRedirect: '/user',
-    //change failureRedirect to what's best for your app
     failureRedirect: '/'
   }
 ));
@@ -37,7 +33,6 @@ router.get('/oauth2callback', passport.authenticate(
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function() {
-    //change path to your landing page (customize route for your project)
     res.redirect('/');
   });
 });
